1379618deda695674e6afbd6766c79af2d465ac5
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-09 11:35:10 +0300

    psw and email validation

--- src/components/InputBlock.jsx
+++ src/components/InputBlock.jsx
@@ -1,35 +1,30 @@
-import { useState } from "react"
+import { useState } from 'react';
 
-function InputBlock() {
-    const [inputVal, setInputVal] = useState('')
-    const [showText, setShowText] = useState(false)
-
+export default function InputBlock() {
+    const [inputVal, setInputVal] = useState('James Bond');
+    const [showText, setShowText] = useState(false);
     function enterInput(event) {
-        console.log('change', event.target.value);
         const inputEl = event.target;
-        const valInInput = inputEl.value;
-        setInputVal(valInInput)
+        const valueInInput = inputEl.value;
+        setInputVal(valueInInput);
     }
 
     function makeClick() {
-        setInputVal('')
+        setShowText(true);
     }
 
-
-
     return (
-        <div className="container">
-            <label htmlFor="input">Input</label>
+        <div>
+            <label htmlFor='input'>Input</label>
             <input
                 onChange={enterInput}
                 value={inputVal}
                 id='input'
-                type="text"
-                placeholder="enter here" />
+                type='text'
+                placeholder='enter here'
+            />
             <button onClick={makeClick}>Show Output</button>
             {showText && <h2>I input buvo ivesta: {inputVal}</h2>}
         </div>
-    )
+    );
 }
-
-export default InputBlock
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,51 +1,96 @@
-// create component
-// import App.jsx
-// form
-// 2 inputs - email & password
-// submit btn
-// email state (onChange value)
-// password state
+import { useState } from 'react';
 
-import { useState } from "react";
+const correctEmail = 'James@bond.com';
+const correctPass = 'secret123';
 
-function Login() {
-    const [emailVal, setEmailVal] = useState('')
-    const [passwordVal, setPasswordVal] = useState('')
+export default function Login() {
+    const [emailVal, setEmailVal] = useState('');
+    const [passVal, setPassVal] = useState('');
+    const [formSuccess, setFormSuccess] = useState(false);
+    const [errorMsg, setErrorMsg] = useState('')
 
-    function getEmail(event) {
+    /**
+     * @param {InputEvent} event
+     */
+    function emailInput(event) {
         setEmailVal(event.target.value);
     }
-    function getPsw(event) {
-        setPasswordVal(event.target.value);
+    function paswInput(event) {
+        setPassVal(event.target.value);
     }
-
+    /**
+     * @param {SubmitEvent} event
+     */
+    function handleSubmit(event) {
+        setErrorMsg('')
+        // sustabdom puslapi nuo perkrovimo
+        event.preventDefault();
+        console.log('submit');
+        // daryti ka reikia
+        // atspausdinti ivestas reikmes:
+        // console.log('ivesta email:', emailVal);
+        // console.log('ivesta password:', passwVal);
+        // validuoti ar ne tusti laukai
+        if (emailVal.trim() === '' || passVal.trim() === '') {
+            console.log('Abu laukai privalomi');
+            return;
+        }
+        // patikrinti ar sutampa email (turetu buti nesvarbu dizioji ar mazoji raides)
+        // patikrinti ar sutampa password
+        // jei nesutampa tai consolinam klaida
+        if (emailVal.toLocaleLowerCase() !== correctEmail.toLocaleLowerCase() ||
+            passVal.toLocaleLowerCase() !== correctPass.toLocaleLowerCase()) {
+            console.log('wrong email or password');
+            setErrorMsg('wrong email or password');
+            return;
+        }
+        // jei nesutampa atvaizduojam klaida <h2 className="errorMsg"></h2>
+        // jei viskas gerai - sekmes atvejis
+        console.log('---Sekme---');
+        setFormSuccess(true);
+    }
+    console.log('formSuccess ===', formSuccess);
     return (
         <div>
-            <form className="container">
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input
-                        onChange={getEmail}
-                        value={emailVal}
-                        type="text"
-                        name="email"
-                        id="email" />
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input
-                        onChange={getPsw}
-                        value={passwordVal}
-                        type="password"
-                        name="password"
-                        id="password" />
-                </div>
-                <button type="submit">Submit</button>
-            </form>
+            {/* {errorMsg !== '' && <h2 className='errorMsg'>{errorMsg}</h2>} */}
+            {errorMsg.length > 0 && <h2 className='errorMsg'>{errorMsg}</h2>}
+            {/* {Boolean(errorMsg.length) && <h2 className='errorMsg'>{errorMsg}</h2>} */}
+            {
+                !formSuccess && (
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <label>Email:</label>
+                            <input
+                                onChange={emailInput}
+                                value={emailVal}
+                                type='text'
+                                name='email'
+                            />
+                        </div>
+                        <div>
+                            <label>Password:</label>
+                            <input
+                                onChange={paswInput}
+                                value={passVal}
+                                type='password'
+                                name='password'
+                            />
+                        </div>
+                        <button type='submit'>Login</button>
+                    </form>
+                )
+            }
+            {
+                formSuccess && (
+                    <div className='success'>
+                        <h2>You have logged in succesfuly</h2>
+                        <p>Welcome back to the website</p>
+                    </div>
+                )
+            }
+            <hr />
             <h3>Email: {emailVal}</h3>
-            <h3>Password: {passwordVal}</h3>
-        </div>
-    )
+            <h3>Password: {passVal}</h3>
+        </div >
+    );
 }
-
-export default Login;
