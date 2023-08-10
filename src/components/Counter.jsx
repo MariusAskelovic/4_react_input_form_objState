9f6b335937e6d84af9824230f45782638d1dc499
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-10 08:44:44 +0300

    theProblemo

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,6 +2,19 @@ import { useState } from "react"
 
 function Counter() {
     const [value, setValue] = useState(0)
+    console.log('susikure');
+
+    function plus2() {
+        setTimeout(() => {
+            setValue(value + 2)
+        }, 3000)
+    }
+    // negalima komponento viduje kviesti
+    // state keiciancios funkcijos
+    // nes turesim infinite loop
+
+    // plus2()
+
     function plusValue() {
         setValue(value + 1)
     }
